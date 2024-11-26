15cdd94277d7dd0caa7b559cff77ee1626e499c5
PorchettaGamer <msheqja3@gmail.com>
2024-11-26 11:49:35 +0100

    primo commit

---
+++ es1.py
@@ -0,0 +1,11 @@
+print("Ciao Beshtia")
+
+import random 
+num1=int(input("Inserisci un numero"))
+num2=random.randint(1,100)
+print(num1)
+print(num2)
+if num1>num2:
+    print("num1 è maggiore")
+else:
+    print("num2 è maggiore")
