b246bd3c87dd0f09e899128958cf1d59e43ac355
PorchettaGamer <msheqja3@gmail.com>
2024-11-26 12:09:58 +0100

    esercizi python

---
+++ cicli.py
@@ -0,0 +1,15 @@
+print("Ciao Beshtia")
+
+import random 
+num1=int(input("Inserisci un numero"))
+num2=random.randint(1,100)
+print(num1)
+print(num2)
+if num1>num2:
+    print("num1 è maggiore")
+else:
+    print("num2 è maggiore")
+for num1 in range(1, 28):
+    print("Minimo raggiunto")
+for num2 in range(29, 100):
+    print("Massimo raggiunto")
---
+++ input.py
@@ -0,0 +1,7 @@
+print("Ciao Beshtia")
+
+import random 
+num1=int(input("Inserisci un numero"))
+num2=random.randint(1,100)
+print(num1)
+print(num2)
